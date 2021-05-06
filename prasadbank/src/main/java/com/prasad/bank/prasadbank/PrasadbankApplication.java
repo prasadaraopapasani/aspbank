c1db91fd66ce61aab3c4c7857198a3733abd2a4d
Papasani <prasada-rao.papasani@capgemini.com>
2021-05-06 13:07:10 -0400

    initial project created

---
+++ prasadbank/src/main/java/com/prasad/bank/prasadbank/PrasadbankApplication.java
@@ -0,0 +1,13 @@
+package com.prasad.bank.prasadbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrasadbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrasadbankApplication.class, args);
+	}
+
+}
---
+++ prasadbank/src/test/java/com/prasad/bank/prasadbank/PrasadbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.bank.prasadbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrasadbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
